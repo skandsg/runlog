5947712a41f25064d3c8ac509ed9c5a1ff761207
Skand Gupta <skands@gmail.com>
2012-05-04 18:52:23 -0400

    Make a basic user model (including secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120422161745) do
+ActiveRecord::Schema.define(:version => 20120427023134) do
 
   create_table "runs", :force => true do |t|
     t.date     "date"
@@ -25,8 +25,11 @@ ActiveRecord::Schema.define(:version => 20120422161745) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
---
+++ test/unit/user_spec_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserSpecTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
